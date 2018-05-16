d14f6ada835b1b8c60c984e523a0fac06727dde5
shannoncantech <LdwpEtegOLLon9#1>
2018-05-16 19:31:31 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String[] myNames = {"Anna", "Brianna", "Charlies", "Douglas"};
+
+	    int x =0;
+	    while(x<4){
+            System.out.println("Hi, my name is " + myNames[x] + "!!!");
+            x++;
+        }
+    }
+}
